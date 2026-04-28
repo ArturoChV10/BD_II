caeda65aa4712d5beba2a92ffb5c50e21d80e42e
Darisoth19 <migueldariosotobadilla@gmail.com>
2026-04-27 20:23:17 -0600

    Refactor database type handling in factory.js

--- restaurant-reservations/src/daos/factory.js
+++ restaurant-reservations/src/daos/factory.js
@@ -1,18 +1,23 @@
-const dbType = process.env.DATABASE_TYPE || 'postgres';
+const dbType = (process.env.DATABASE_TYPE || "postgres").toLowerCase();
 
-if (dbType === 'postgres') {
+if (["postgres", "postgresql", "pg"].includes(dbType)) {
   module.exports = {
-    userDao: require('./postgres/userDaoPg'),
-    restaurantDao: require('./postgres/restaurantDaoPg'),
-    menuDao: require('./postgres/menuDaoPg'),
-    dishDao: require('./postgres/dishDaoPg'),
-    reservationDao: require('./postgres/reservationDaoPg'),
-    orderDao: require('./postgres/orderDaoPg')
-    // Más adelante agregaremos restaurantDao, menuDao, etc.
+    userDao: require("./postgres/userDaoPg"),
+    restaurantDao: require("./postgres/restaurantDaoPg"),
+    menuDao: require("./postgres/menuDaoPg"),
+    dishDao: require("./postgres/dishDaoPg"),
+    reservationDao: require("./postgres/reservationDaoPg"),
+    orderDao: require("./postgres/orderDaoPg")
   };
-} else if (dbType === 'mongodb') {
-  // Por ahora lanzamos un error (lo implementaremos después)
-  throw new Error('Aquí se implementaría Mongo');
+} else if (["mongodb", "mongo"].includes(dbType)) {
+  module.exports = {
+    userDao: require("./mongodb/userDaoMongo"),
+    restaurantDao: require("./mongodb/restaurantDaoMongo"),
+    menuDao: require("./mongodb/menuDaoMongo"),
+    dishDao: require("./mongodb/dishDaoMongo"),
+    reservationDao: require("./mongodb/reservationDaoMongo"),
+    orderDao: require("./mongodb/orderDaoMongo")
+  };
 } else {
   throw new Error(`Base de datos desconocida: ${dbType}`);
 }
